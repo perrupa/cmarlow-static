d1f8ee8c1df9563bb9f668cad3987bd02c9757e7
Chris Marlow <christopher.marlow@shopify.com>
2017-10-22 21:53:38 -0400

    Adds initial index and post/page

---
+++ src/pages/high-park.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Photos = ({ photos }) =>
+  <div>
+    { photos.map(photo =>
+        <img
+          src={`/images/high-park/${photo}`}
+        />)}
+  </div>
+
+const photos = [
+  'DSCF6519.jpg',
+  'DSCF6557.jpg',
+  'DSCF6561.jpg',
+  'DSCF6587-2.jpg',
+  'DSCF6632-2.jpg',
+  'DSCF6555.jpg',
+  'DSCF6559.jpg',
+  'DSCF6566.jpg',
+  'DSCF6598-2.jpg',
+  'DSCF6645-2.jpg',
+]
+
+const SecondPage = () => (
+  <div>
+    <h1>A walk in High Park</h1>
+    <Photos photos={photos} />
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome</h1>
+    <Link to="/high-park/">Some photos that I took.</Link>
+  </div>
+)
+
+export default IndexPage
