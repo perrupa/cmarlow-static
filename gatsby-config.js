7c9a54dd51058c0549a48c20ea614b55eed0a0c3
Chris Marlow <christopher.marlow@shopify.com>
2017-10-28 19:01:08 -0400

    committing updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  pathPrefix: '/cmarlow-static',
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: 'Gatsby Default Starter',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: ['gatsby-plugin-react-helmet'],
 }
