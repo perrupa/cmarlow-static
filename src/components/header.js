06d40bf07e2995211afafa65aef2823b39393b5e
Chris Marlow <christopher.marlow@shopify.com>
2017-10-31 01:43:57 -0400

    splits out a header component

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default () => (
+  <div style={{textAlign: 'center'}}>
+    <h1 style={{
+      margin: ".3em",
+    }}>
+      <Link to="/" >
+        CMarlow
+      </Link>
+    </h1>
+  </div>
+)
+
+
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,54 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 import './index.css'
-
-const Header = () => (
-  <div
-    style={{
-      background: '#333',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          CMarlow
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import Header from '../components/header'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="C:\Marlow>"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: '' },
+        { name: 'keywords', content: '' },
       ]}
     />
     <Header />
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        maxWidth: 1024,
       }}
     >
       {children()}
