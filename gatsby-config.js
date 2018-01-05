baf4f8a0c5f7af95150d6032909f7263d4d88c91
Chris Marlow <christopher.marlow@shopify.com>
2018-01-05 01:20:32 -0500

    Adding the ability to read markdown posts from a posts/ folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
     author: 'Chris Marlow',
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: "posts",
+      },
+    },
+    'gatsby-transformer-remark',
     `gatsby-plugin-sass`,
     'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet'
